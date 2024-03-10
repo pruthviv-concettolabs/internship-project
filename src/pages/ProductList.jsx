c40005ade7111b0e84fe5189755e06dbfc4f5e6a
pruthviv-concettolabs <--edit>
2024-03-10 18:14:04 +0530

    productlist created

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,5 +1,33 @@
+import { Col, Row, Container, Breadcrumb } from "react-bootstrap";
+
 const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <>
+      <section className="product-list">
+        <Container>
+          <Row>
+            <Breadcrumb>
+              <Breadcrumb.Item
+                href="#"
+                className="text-muted text-decoration-none">
+                Home
+              </Breadcrumb.Item>
+              <Breadcrumb.Item
+                href="/#"
+                className="text-muted text-decoration-none">
+                Library
+              </Breadcrumb.Item>
+              <Breadcrumb.Item active>Data</Breadcrumb.Item>
+            </Breadcrumb>
+          </Row>
+          <Row>
+            <Col className="col-lg-4">1</Col>
+            <Col className="col-lg-8">2</Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default ProductList;

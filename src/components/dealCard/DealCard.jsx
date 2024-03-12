dd9e1eaea74579c95038cb37fc6d354f5eb0bd70
pruthviv-concettolabs <--edit>
2024-03-12 19:05:05 +0530

    link added on product card

--- src/components/dealCard/DealCard.jsx
+++ src/components/dealCard/DealCard.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom";
 import "./dealCard.scss";
 import { Card } from "react-bootstrap";
 const DealCard = ({ id, imgUrl, discount, title }) => {
   return (
-    <Card key={id} className="d-flex p-0 h-100 ">
-      <Card.Img src={imgUrl} alt="" />
-      <Card.Body className="h-100 m-0">
-        <Card.Text>{discount}</Card.Text>
-        <Card.Title className="mt-auto">{title}</Card.Title>
-      </Card.Body>
-    </Card>
+    <Link to={`/productList`}>
+      <Card key={id} className="d-flex p-0 h-100 ">
+        <Card.Img src={imgUrl} alt="" />
+        <Card.Body className="h-100 m-0">
+          <Card.Text>{discount}</Card.Text>
+          <Card.Title className="mt-auto">{title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 

a2fd4ec3c8f3fb360478da65fad7a35dd4c8ddbe
pruthviv-concettolabs <--edit>
2024-03-12 19:04:40 +0530

    productdetail page added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import ProductList from "./pages/Productlist/ProductList";
 import Footer from "./components/footer/Footer";
 import Login from "./pages/login/Login";
-
+import ProductDetail from "./pages/productDetail/ProductDetail";
 const App = () => {
   return (
     <>
@@ -14,7 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productList" element={<ProductList />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/productDetail" element={<ProductDetail />} />
       </Routes>
       <Footer />
     </>

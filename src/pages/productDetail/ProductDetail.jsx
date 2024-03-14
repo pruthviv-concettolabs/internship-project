64e87f7af0b1f5f8803082d07998acb4240f71fe
pruthviv-concettolabs <--edit>
2024-03-14 19:08:48 +0530

    right col css change

--- src/pages/productDetail/ProductDetail.jsx
+++ src/pages/productDetail/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import "./productDetail.scss";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import share from "../../img/share.png";
-
+import Ruler from "../../img/Ruler.png";
 import Star from "../../components/star/Star";
 
 const ProductDetail = () => {
@@ -28,6 +28,7 @@ const ProductDetail = () => {
                   <img src={share} alt="share img" />
                 </div>
               </div>
+              {/* second line  */}
               <div className="rating d-flex  justify-content-between ">
                 <div>
                   <Star />
@@ -38,6 +39,53 @@ const ProductDetail = () => {
                   <strong>Celby Store</strong>
                 </div>
               </div>
+              {/* third line  */}
+              <div className="price-list">
+                <div className="price">
+                  <div className="d-flex align-items-start ">
+                    <span className="currency-type"> $</span>
+                    <span className="currency">12</span>
+                  </div>
+                  <span className="real-price">
+                    <del> $38.98</del>
+                  </span>
+                  <div className="discount  rounded-circle">
+                    <span>65% off</span>
+                  </div>
+                </div>
+              </div>
+              {/* fourth line */}
+              <div className="color-filter">
+                <div className="color-title mb-2">
+                  color: <strong>Orange</strong>
+                </div>
+                <div className="color-options">
+                  <Button style={{ backgroundColor: "#F76F3D" }}></Button>
+                  <Button style={{ backgroundColor: "#000000" }}></Button>
+                  <Button style={{ backgroundColor: "#327E07" }}></Button>
+                  <Button style={{ backgroundColor: "#8185E8" }}></Button>
+                  <Button style={{ backgroundColor: "#1B3497" }}></Button>
+                </div>
+              </div>
+              {/* fifth line */}
+              <div className="size-group mb-3">
+                <div className="size-chart">
+                  <span>Size:</span>
+                  <Button>XS</Button>
+                  <Button>S</Button>
+                  <Button>M</Button>
+                  <Button>L</Button>
+                  <Button>XL</Button>
+                  <Button>XXL</Button>
+                </div>
+                <span className="size-guide">
+                  <Image src={Ruler} />
+                  Size Guide
+                </span>
+              </div>
+              {/* sixth row  */}
+
+              <div className="quantity-box">kl</div>
             </div>
           </Col>
         </Row>

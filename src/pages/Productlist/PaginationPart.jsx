74d9c7252964e5842f4ccd6c9dff91ae9681a01f
pruthviv-concettolabs <--edit>
2024-03-22 15:25:01 +0530

    pagination added

--- src/pages/Productlist/PaginationPart.jsx
+++ src/pages/Productlist/PaginationPart.jsx
@@ -13,7 +13,6 @@ const PaginationPart = () => {
           <Pagination.Item active>{1}</Pagination.Item>
           <Pagination.Item>{2}</Pagination.Item>
           <Pagination.Item>{3}</Pagination.Item>
-          <Pagination.Item>{4}</Pagination.Item>
           <Pagination.Ellipsis />
           <Pagination.Item>{11}</Pagination.Item>
           <Pagination.Item>{12}</Pagination.Item>
